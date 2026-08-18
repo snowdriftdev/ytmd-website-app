f57971eaf6cb2703e3f1c12e7c9e1c1d208dd422
Carter <github@mail.snowdrift.sh>
2026-08-18 17:25:41 -0600

    add parsing for the json response so it only returns active song rather than the whole queue

--- get-state-loop.py
+++ get-state-loop.py
@@ -16,10 +16,23 @@ headers = {"Authorization": f"{token}"}
 
 def getState():
     currentState = requests.get("http://localhost:9863/api/v1/state", headers=headers)
-    print(currentState.text)
+    #print(currentState.text)
     # unminify the json and export it to the log file
-    logging.debug(json.dumps(json.loads(currentState.text), indent=4))
+    #logging.debug(json.dumps(json.loads(currentState.text), indent=4))
+    return(currentState.text)
+
+def parseStateForActiveSong(state):
+    jsonState = json.loads(state)
+    for item in jsonState["player"]["queue"]["items"]:
+        if item["selected"]:
+            logging.debug(json.dumps(item, indent=4))
+            # returns just the currently playing song from the queue
+            return(item)
+    print("ERROR: No current song found.")
+    return("")
+
 
 while True:
-    getState()
+    # converts the result to a formatted json dump and prints it
+    print(json.dumps(parseStateForActiveSong(getState()), indent=4))
     time.sleep(5)
